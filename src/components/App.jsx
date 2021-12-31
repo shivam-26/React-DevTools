86df945e840556f1b254738f4839597f2b278052
shivam-26 <79416882+shivam-26@users.noreply.github.com>
2021-12-31 11:28:48 +0530

    React DevTools

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
+      <Avatar img="https://scontent.fdel29-1.fna.fbcdn.net/v/t1.6435-9/77178852_155649109034793_5103858930900008960_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=SGZti8mNKo8AX8sFGvL&_nc_ht=scontent.fdel29-1.fna&oh=00_AT_uvCL_DR_nRsbQoXiXY0KUZyquC2HXEbIWLRgnXJv0Mw&oe=61F49976" />
       <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
