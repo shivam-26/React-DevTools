4aa23172b88dd26f27f2036c7b58d806e760011d
shivam-26 <79416882+shivam-26@users.noreply.github.com>
2021-12-31 11:52:35 +0530

    Mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,27 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+  return (
+    <Card
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://scontent.fdel29-1.fna.fbcdn.net/v/t1.6435-9/77178852_155649109034793_5103858930900008960_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=SGZti8mNKo8AX8sFGvL&_nc_ht=scontent.fdel29-1.fna&oh=00_AT_uvCL_DR_nRsbQoXiXY0KUZyquC2HXEbIWLRgnXJv0Mw&oe=61F49976" />
-      <Card
+
+      {contacts.map(createCard)}
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -25,7 +40,7 @@ function App() {
         img={contacts[2].imgURL}
         tel={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
